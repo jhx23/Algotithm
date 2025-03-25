7385aaf3022ca7cf7f5a34010368301093e9b99b
jh <biruk23@naver.com>
2025-03-25 16:46:25 +0900

    알고리즘 공부

---
+++ src/main/java/BaekJoon/Practice.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+/* 브론즈5 풀어보는 용*/
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
