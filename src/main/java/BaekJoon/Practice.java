c553252e5b6b0d575ebcc1f8b8611ec0a4c7255b
jh <biruk23@naver.com>
2025-04-03 15:37:10 +0900

    알고리즘 공부

--- src/main/java/BaekJoon/Practice.java
+++ src/main/java/BaekJoon/Practice.java
@@ -1,12 +1,12 @@
 package BaekJoon;
 
 /* 브론즈5 풀어보는 용*/
+import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+        Scanner s = new Scanner(System.in);
+        int year = s.nextInt();
+        System.out.println((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 1 : 0);
     }
 }
