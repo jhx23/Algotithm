1eaa51ef27e0df470d13ee7e6a34cdef173d4928
jh <biruk23@naver.com>
2025-04-30 14:13:41 +0900

    알고리즘 공부

--- src/main/java/BaekJoon/Practice.java
+++ src/main/java/BaekJoon/Practice.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int count = N / 4;
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            sb.append("long ");
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+            System.out.println(A + B);
         }
-        sb.append("int");
-
-        System.out.println(sb.toString());
     }
 }
